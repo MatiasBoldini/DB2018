a4ea0c28c163ee3af16ca85a31e00b2ebac9c48a
MatiasBoldini <matiboldini2000@gmail.com>
2018-05-11 14:25:36 -0300

    ejercicio 3 y 4 de la clase 11

--- Clase11.sql
+++ Clase11.sql
@@ -20,3 +20,23 @@ ORDER BY COUNT(city) DESC;
 
 3)
 
+SELECT customer.first_name, customer.last_name,
+		(SELECT address.address
+		FROM address
+		WHERE customer.address_id = address.address_id) AS address,
+		COUNT(*) AS rents,
+		(SELECT SUM(amount)
+		FROM payment
+		WHERE payment.customer_id = customer.customer_id) AS amount
+FROM customer
+INNER JOIN rental
+	ON rental.customer_id = customer.customer_id
+GROUP BY customer.customer_id
+ORDER BY amount DESC;
+
+
+4)
+
+SELECT film_id, title
+FROM film
+WHERE film_id NOT IN (SELECT film_id FROM inventory);
