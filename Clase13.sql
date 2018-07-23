51a113ab887de7b8e3edb02068c43edbb111e829
MatiasBoldini <matiboldini2000@gmail.com>
2018-07-23 15:57:46 -0300

    Clase 13

--- Clase13.sql
+++ Clase13.sql
@@ -12,6 +12,16 @@ WHERE country.country LIKE 'United States'
 ORDER BY address_id DESC LIMIT 1));
 
 
+2)
+
+INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id) 
+VALUES
+(CURRENT_DATE(),
+(SELECT inventory_id FROM inventory INNER JOIN film USING (film_id) WHERE title = 'COWBOY DOOM' LIMIT 1),
+(SELECT customer_id FROM customer WHERE store_id = 1 LIMIT 1),
+(SELECT staff_id FROM staff WHERE store_id = 1 LIMIT 1));
+
+
 3)
 
 UPDATE film
