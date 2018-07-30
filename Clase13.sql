abbbd3d03fbf401821c2581f84fca005c541c140
MatiasBoldini <matiboldini2000@gmail.com>
2018-07-30 13:42:28 -0300

    Actualizacion clase 13

--- Clase13.sql
+++ Clase13.sql
@@ -45,3 +45,64 @@ SET release_year = '2005'
 WHERE rating = 'NC-17';
 
 
+4-
+
+SELECT MAX(rental_id) FROM film
+INNER JOIN inventory USING (film_id)
+INNER JOIN rental t USING (inventory_id)
+WHERE t.return_date IS NULL;
+
+UPDATE rental
+SET return_date = CURRENT_TIMESTAMP
+WHERE rental_id = 15966;
+
+
+5-
+
+#Seleccionamos la pelicula.
+SELECT * FROM film LIMIT 1;
+
+#Eliminamos todos los registros que tengan esa pelicula de la tabla 'film_category'.
+DELETE FROM film_category WHERE film_id = (SELECT film_id FROM film LIMIT 1);
+
+#Eliminamos todos los registros que tengan esa pelicula de la tabla 'film_actor'.
+DELETE FROM film_actor WHERE film_id = (SELECT film_id FROM film LIMIT 1);
+
+#Eliminamos todos los registros de rentas de los inventarios que pertenezcan a nuesta pelicula.
+DELETE FROM rental WHERE inventory_id IN
+(SELECT inventory_id FROM inventory WHERE film_id = (SELECT film_id FROM film LIMIT 1));
+
+#Eliminamos todos los inventarios de la pelicula.
+DELETE FROM inventory WHERE film_id = (SELECT film_id FROM film LIMIT 1);
+
+#Finalmente eliminamos la pelicula.
+DELETE FROM film WHERE film_id = 1;
+
+
+6-
+
+SELECT inventory_id
+FROM inventory
+INNER JOIN rental USING (inventory_id)
+WHERE rental.return_date IS NOT NULL
+AND inventory.store_id = 1;
+
+
+INSERT INTO rental
+(rental_date, inventory_id, customer_id, staff_id)
+VALUES(
+CURRENT_DATE(),
+10,
+(SELECT customer_id FROM customer ORDER BY customer_id DESC LIMIT 1),
+(SELECT staff_id FROM staff WHERE store_id = (SELECT store_id FROM inventory WHERE inventory_id = 10))
+);
+
+
+INSERT INTO payment
+(customer_id, staff_id, rental_id, amount, payment_date)
+VALUES(
+(SELECT customer_id FROM customer ORDER BY customer_id DESC LIMIT 1),
+(SELECT staff_id FROM staff LIMIT 1),
+(SELECT rental_id FROM rental ORDER BY rental_id DESC LIMIT 1) ,
+(SELECT rental_rate FROM film WHERE film_id = 2),
+CURRENT_DATE());
